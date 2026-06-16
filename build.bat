c11298469db667b5acf9b47734cbec7dfd5318e6
hugo562-a11y <hugo562@gmail.com>
2026-06-17 01:18:48 +0800

    Fix build.bat: use python -m PyInstaller instead of pyinstaller command

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building StreamMouse.exe ...
-pyinstaller stream_mouse.spec --clean
+python -m PyInstaller stream_mouse.spec --clean
 echo.
 if exist "dist\StreamMouse.exe" (
     echo Done!  dist\StreamMouse.exe
